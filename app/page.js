7a379af36aa7a8236ca47ca5af914052029552f6
shivlahejat <shivlahejat123@gmail.com>
2025-08-11 12:14:43 +0530

    task: optimise strucure

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import LoginPage from "@/imports/Auth/Pages/LoginPage";
-import Signup from "@/imports/Auth/Pages/Signup";
 import React from "react";
 
 const page = () => {
-  return <Signup />;
+  return <div>Landing Page</div>;
 };
 
 export default page;
---
+++ app/signup/page.js
@@ -0,0 +1,8 @@
+import MainLoginSection from "@/imports/Login/ui/component/MainLoginSection";
+import React from "react";
+
+const page = () => {
+  return <MainLoginSection />;
+};
+
+export default page;
